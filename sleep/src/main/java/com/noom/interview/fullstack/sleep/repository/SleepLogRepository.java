dde0cc0b28f8c028ccda3197c952b1a0e72e27d6
Victor Araújo <Victor.Araujo@qikserve.com>
2024-10-26 15:58:58 -0300

    sleep-log-stats Implement util methods for querying past 30 days stats

--- sleep/src/main/java/com/noom/interview/fullstack/sleep/repository/SleepLogRepository.java
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/repository/SleepLogRepository.java
@@ -1,13 +1,20 @@
 package com.noom.interview.fullstack.sleep.repository;
 
+import com.noom.interview.fullstack.sleep.model.LastMonthAveragesResult;
 import com.noom.interview.fullstack.sleep.model.SleepLog;
 import com.noom.interview.fullstack.sleep.model.SleepQuality;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.util.CollectionUtils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.Set;
 
 public interface SleepLogRepository extends JpaRepository<SleepLog, Long> {
@@ -36,4 +43,32 @@ public interface SleepLogRepository extends JpaRepository<SleepLog, Long> {
     @Query("SELECT s.quality as quality, count(*) as count FROM SleepLog s WHERE s.userId = :userId AND s.startTime >= :initialDate AND s.endTime < :finalDate GROUP BY s.quality")
     Set<SleepQualityCount> getSleepQualityCount(@Param("userId") Long userId, @Param("initialDate") LocalDateTime initialDate, @Param("finalDate") LocalDateTime finalDate);
 
+    default LastMonthAveragesResult getLastMonthAverages(Long userId, LocalDate referenceDate) {
+        LocalDate initialDate = referenceDate.minusDays(30);
+        AveragesResult result = getAverages(userId, initialDate.atStartOfDay(), referenceDate.atTime(LocalTime.MAX));
+        if (Objects.isNull(result)) {
+            return null;
+        }
+        return new LastMonthAveragesResult(
+            result.getAverageTotalTimeSeconds(),
+            result.getAverageStartTime(),
+            result.getAverageEndTime(),
+            initialDate,
+            referenceDate
+        );
+    }
+
+    default Map<SleepQuality,Long> getLastMonthSleepQualityCount(Long userId, LocalDate referenceDate) {
+        LocalDate initialDate = referenceDate.minusDays(30);
+        Set<SleepQualityCount> result = getSleepQualityCount(userId, initialDate.atStartOfDay(), referenceDate.atTime(LocalTime.MAX));
+        Map<SleepQuality, Long> sleepQualityCount = new HashMap<>(Map.of(
+            SleepQuality.BAD, 0L,
+            SleepQuality.OK, 0L,
+            SleepQuality.GOOD, 0L
+        ));
+        if (!CollectionUtils.isEmpty(result)) {
+            result.forEach(c -> sleepQualityCount.put(c.getQuality(), c.getCount()));
+        }
+        return sleepQualityCount;
+    }
 }
