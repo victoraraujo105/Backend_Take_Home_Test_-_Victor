e756c52551523e0c5b8f7b092283250baefa52ba
Victor Araújo <Victor.Araujo@qikserve.com>
2024-10-26 15:22:50 -0300

    sleep-log-model Create sleep log entity

---
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/model/SleepLog.java
@@ -0,0 +1,47 @@
+package com.noom.interview.fullstack.sleep.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sleep_logs")
+@TypeDef(name = "pgsql_enum", typeClass = PostgreSQLEnumType.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SleepLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @Enumerated(EnumType.STRING)
+    @Type(type = "pgsql_enum")
+    @Column(name = "quality", nullable = false)
+    private SleepQuality quality;
+}
