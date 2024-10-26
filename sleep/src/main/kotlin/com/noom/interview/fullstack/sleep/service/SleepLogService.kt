6485a49e1843e2c2dd8ac49e5582a273113f6501
Victor Araújo <Victor.Araujo@qikserve.com>
2024-10-26 19:39:13 -0300

    get-last-month-stats Add service method

--- sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/service/SleepLogService.kt
+++ sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/service/SleepLogService.kt
@@ -2,9 +2,12 @@ package com.noom.interview.fullstack.sleep.service
 
 import com.noom.interview.fullstack.sleep.model.SleepLog
 import com.noom.interview.fullstack.sleep.model.User
+import com.noom.interview.fullstack.sleep.model.dto.SleepLogLastMonthStatsResponse
 import com.noom.interview.fullstack.sleep.model.dto.SleepLogLastNightCreateRequest
 import com.noom.interview.fullstack.sleep.repository.SleepLogRepository
 import org.springframework.stereotype.Service
+import java.time.Duration
+import java.time.LocalDate
 
 @Service
 class SleepLogService(
@@ -14,5 +17,19 @@ class SleepLogService(
     fun saveLastNight(request: SleepLogLastNightCreateRequest): SleepLog {
         val sleepLog = request.toEntity(adminUser)
         return sleepLogRepository.save(sleepLog)
+    }
+
+    fun getLastMonthStats(): SleepLogLastMonthStatsResponse {
+        val today = LocalDate.now()
+        val lastMonthAverages = sleepLogRepository.getLastMonthAverages(adminUser.id, today)
+        val lasMonthSleepQualityCount = sleepLogRepository.getLastMonthSleepQualityCount(adminUser.id, today)
+        return SleepLogLastMonthStatsResponse(
+            averageTotalTime = Duration.ofSeconds(lastMonthAverages.averageTotalTimeSeconds),
+            averageWakeUpTime = lastMonthAverages.averageStartTime,
+            averageSleepTime = lastMonthAverages.averageEndTime,
+            initialDate = lastMonthAverages.initialDate,
+            finalDate = lastMonthAverages.finalDate,
+            sleepQualityCount = lasMonthSleepQualityCount
+        )
     }
 }
