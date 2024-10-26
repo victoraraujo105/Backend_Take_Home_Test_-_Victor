1fd03cf6c4054ab1d283e665b50f037f358d2aed
Victor Araújo <Victor.Araujo@qikserve.com>
2024-10-26 14:07:02 -0300

    user-model Create user entity

---
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/model/User.java
@@ -0,0 +1,37 @@
+package com.noom.interview.fullstack.sleep.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
